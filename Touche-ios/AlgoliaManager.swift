c2d1cb08b51a9132218f758ed9dcd397e0fb93d5
Lucas Maris <lucas.m61523@gmail.com>
2018-03-16 12:33:11 -0500

    - some UI updates

---
+++ Touche-ios/AlgoliaManager.swift
@@ -0,0 +1,18 @@
+//
+//  AlgoliaManager.swift
+//  Touche-ios
+//
+//  Created by Marcus Korpi on 7/7/16.
+//  Copyright © 2017 127Thousand LLC. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class AlgoliaManager {
+
+    // MARK: Properties
+
+    static let sharedIntance = AlgoliaManager()
+
+}
---
+++ Touche-ios/MessageTypeProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  MessageTypeProtocol.swift
+//  Touche-ios
+//
+//  Created by Marcus Korpi on 15/9/16.
+//  Copyright © 2017 127Thousand LLC. All rights reserved.
+//
+
+import Foundation
+
+protocol MessageTypeProtocol {
+    
+    func tapOnMessageType(_ item:String)
+    
+}
